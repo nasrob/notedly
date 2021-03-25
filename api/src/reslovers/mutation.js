ac9aa3745a140f73061a83cc7565c5626944d3c0
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-03-25 01:30:56 +0100

    implemented signup method

--- api/src/reslovers/mutation.js
+++ api/src/reslovers/mutation.js
@@ -52,4 +52,28 @@ module.exports = {
             throw new Error('Error creating account');
         }
     },
+
+    signIn: async (parent, { username, email, password }, { models }) => {
+        if (email) {
+            email = email.trim().toLowerCase();
+        }
+
+        const user = await models.User.findOne({
+            $or: [{ email }, { username }]
+        });
+
+        // if no user is found, throw an authentication error
+        if (!user) {
+            throw new AuthenticationError('Error signing in');
+        }
+
+        // if the passord don't match, throw an authentication error
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new AuthenticationError('Error signing in');
+        }
+
+        // create and return the jwt
+        return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    }
 };
