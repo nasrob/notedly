cf1593425f78d31e27b8ab2dee84593255d26488
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-03-25 01:18:19 +0100

    added signup method and gravatar util

--- api/src/reslovers/mutation.js
+++ api/src/reslovers/mutation.js
@@ -1,3 +1,10 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { AuthenticationError, ForbiddenError } = require('apollo-server-express')
+require('dotenv').config;
+
+const gravatar = require('../util/gravatar')
+
 module.exports = {
     newNote: async (parent, args, { models }) => {
         return await models.Note.create({
@@ -21,5 +28,28 @@ module.exports = {
             { $set: { content } },
             { new: true }
         );
+    },
+
+    signUp: async (parent, { username, email, password }, { models }) => {
+        email = email.trim().toLowerCase(); // normalize email
+        const hashed = await bcrypt.hash(password, 10); // hash the password
+        // create the gravatar url
+        const avatar = gravatar(email);
+
+        try {
+            const user = await models.User.create({
+                username,
+                email,
+                avatar,
+                password: hashed
+            });
+
+            // create and return the JWT
+            return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        } catch (err) {
+            console.error(err);
+            // in case of failure
+            throw new Error('Error creating account');
+        }
     },
 };
