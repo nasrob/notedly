27ecf98f924672543717da6dd8a9abbe25447082
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-03-28 17:32:16 +0200

    added users gql queries

--- api/src/reslovers/query.js
+++ api/src/reslovers/query.js
@@ -4,5 +4,20 @@ module.exports = {
     },
     note: async (parent, args, { models }) => {
         return await models.Note.findById(args.id);
+    },
+    user: async (parent, { username }, { models }) => {
+        // find a user by username
+        return await models.User.findOne({ username });
+    },
+    users: async (parent, args, { models }) => {
+        // find all users
+        return await models.User.find({});
+    },
+    me: async (parent, args, { models, user }) => {
+        // find user by current user context
+        return await models.User.findById(user.id);
     }
+
+
+
 };
